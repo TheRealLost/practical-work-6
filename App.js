6a46bee1e45e5a2c61c67a97e5d7231de0617cfa
TheRealLost <viprin07@gmail.com>
2026-05-29 20:06:30 +0300

    Склейка файлов, настройка ссылок

--- App.js
+++ App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import HabitList from './components/HabitList';
+import AddHabit from './components/AddHabit';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const Stack = createNativeStackNavigator();
+
+export default function App() {
+    return (
+        <NavigationContainer>
+            <Stack.Navigator initialRouteName="HabitList">
+                <Stack.Screen
+                    name="HabitList"
+                    component={HabitList}
+                    options={{ title: 'Мои привычки' }}
+                />
+                <Stack.Screen
+                    name="AddHabit"
+                    component={AddHabit}
+                    options={{ title: 'Добавить привычку' }}
+                />
+            </Stack.Navigator>
+        </NavigationContainer>
+    );
 }
-
-export default App;
